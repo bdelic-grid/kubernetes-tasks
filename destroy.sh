56be4cf999861d0ca4bc8da4fa8ec76908f06f6b
Bosko Delic <bdelic@griddynamics.com>
2025-03-20 10:28:33 +0100

    [add] start and destroy scripts

---
+++ destroy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+kubectl delete service petclinic-load-balancer
+kubectl delete deployment petclinic-deployment
+
+gcloud container clusters delete bdelic-cluster --zone=us-central1-a
+
+terraform destroy --auto-approve
---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+terraform apply --auto-approve
+sleep 15
+
+gcloud container clusters create bdelic-cluster --zone=us-central1-a --num-nodes=2 --network=bdelic-kubernetes-vpc --subnetwork=bdelic-kubernetes-subnet
+gcloud container clusters get-credentials bdelic-cluster --location=us-central1-a
+sleep 30
+
+kubectl apply -f ./deployment.yml
+kubectl apply -f ./load_balancer.yml
