6dd51a0937470c928431fb16e4ad9e82ffdc269e
Bosko Delic <bdelic@griddynamics.com>
2025-03-24 15:35:01 +0100

    [refactor] helm values

--- destroy.sh
+++ destroy.sh
@@ -2,9 +2,6 @@
 
 helm delete my-chart
 
-kubectl delete service petclinic-load-balancer
-kubectl delete deployment petclinic-deployment
-
 gcloud container clusters delete bdelic-cluster --zone=us-central1-a
 
 terraform destroy --auto-approve
